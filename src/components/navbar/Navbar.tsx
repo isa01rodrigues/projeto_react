02af22cc98279b1acd2e0cc98f3b0ffd6550b734
Isabella Rodrigues <123326628+isa01rodrigues@users.noreply.github.com>
2026-07-02 15:22:46 -0300

    criação da seção postagem

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,18 +1,17 @@
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { AuthContext } from "../../contexts/AuthContext"
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
-
   const navigate = useNavigate();
 
-    const { handleLogout } = useContext(AuthContext)
+  const { handleLogout } = useContext(AuthContext);
 
-    function logout() {
-
-        handleLogout()
-        alert('O Usuário foi desconectado com sucesso!')
-        navigate('/')}
+  function logout() {
+    handleLogout();
+    alert("O Usuário foi desconectado com sucesso!");
+    navigate("/");
+  }
 
   return (
     <>
@@ -24,13 +23,19 @@ function Navbar() {
             Blog Pessoal
           </Link>
           <div className="flex gap-4">
-            Postagens
-             <Link to='/temas' className='hover:underline'>Temas</Link>
-             <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
-
-                Perfil 
-                
-            <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+            <Link to="/postagens" className="hover:underline">
+              Postagens
+            </Link>
+            <Link to="/temas" className="hover:underline">
+              Temas
+            </Link>
+            <Link to="/cadastrartema" className="hover:underline">
+              Cadastrar tema
+            </Link>
+            Perfil
+            <Link to="" onClick={logout} className="hover:underline">
+              Sair
+            </Link>
           </div>
         </div>
       </div>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,10 @@
+import ListaPostagens from "../../components/postagem/listapostagens/ListaPostagens";
+import ModalPostagem from "../../components/postagem/modalpostagem/ModalPostagem";
+
 function Home() {
   return (
     <>
-      <div className="bg-indigo-500 flex justify-content">
+      <div className="bg-indigo-500 flex justify-center">
         <div className="container grid grid-cols-2 text-white">
           <div className="flex flex-col gap-4 items-center justify-center py-4">
             <h2 className="text-5xl font-bold">Seja Bem Vindo(a)</h2>
@@ -9,8 +12,7 @@ function Home() {
             <p>Expresse aqui seus pensamentos e opiniões</p>
 
             <div className="flex justify-around gap-4">
-              <div className="rounded text-white border-white border-solid border-2 py-2 px-4">
-                Nova Postagem</div>
+              <ModalPostagem />
             </div>
           </div>
 
@@ -23,6 +25,8 @@ function Home() {
           </div>
         </div>
       </div>
+
+      <ListaPostagens />
     </>
   );
 }
